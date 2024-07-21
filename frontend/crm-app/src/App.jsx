ccb3007e45eb09fd0779e269f5644f7fc594db2a
Raffy <raffyabdillah03@gmail.com>
2024-07-21 19:32:20 +0700

    Nambahin sedikit buat setup

--- frontend/crm-app/src/App.jsx
+++ frontend/crm-app/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import Home from './pages/home/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/home/Login';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home />} />
+      <Route path='/login' exact element={<Login />} />
+    </Routes>
+  </Router>
+);
 
 const App = () => {
-  return (
-    <div>
-        <Home />
-    </div>
-  )
+  return <div>{routes}</div>
 }
 
 export default App
--- frontend/crm-app/src/pages/home/Home.jsx
+++ frontend/crm-app/src/pages/home/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-full flex px-12 py-3 drop-shadow-md bg-slate-400'>
+      <div>Home</div>
+      <hr></hr>
+    </div>
   )
 }
 

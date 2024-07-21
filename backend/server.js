aa7b5ce3dfabb5b4c276a9f2594248e1fef43c39
Muhammad Mizan Al Mujadid <111039118+janbu12@users.noreply.github.com>
2024-07-21 22:24:24 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/api', routes);
+app.use('/api', routes());
   
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

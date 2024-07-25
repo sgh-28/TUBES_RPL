b0403ce71a8e20d463433bcba5bb21717128a586
singgit rendang <singgihbau@gmail.com>
2024-07-25 16:18:48 +0700

    ganti div

--- frontend/crm-app/src/pages/home/Home.jsx
+++ frontend/crm-app/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@
 const Home = () => {
   return (
     <div className='w-full flex px-12 py-3 drop-shadow-md bg-slate-400'>
-      <div>Home</div>
+      <div>HOME</div>
       <hr></hr>
     </div>
   )
